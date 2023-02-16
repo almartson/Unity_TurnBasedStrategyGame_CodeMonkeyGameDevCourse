6a05c892a8536e296fd012fa6e581e5373e078b7
AlMartson <6304456+almartson@users.noreply.github.com>
2023-02-15 23:55:00 -0500

    * Added some more Classes (and Functions) Documentation, to make the code more readable and the general purpose of each Class more understandable.

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -148,20 +148,22 @@ public class CameraManager : MonoBehaviour
                 // Vector3 shootDirection = shootAction.AimDirection;
                 
                 // Calculate:  an small Offset to the Camera Rotation, to look in a 3RD CAMERA PERSPECTIVE towards the Target & Shooter together:
+                // This is the 'Unit shoulder's lenght'... because we want to displace the Camera View (from the center of the Character's Neck...) to the right shoulder:
+                // NOTE: although the exact value, with the current Scale of (1, 1, 1), is: 0.34f.
                 //
                 float shoulderOffsetAmount = 0.5f;
                 //
                 // Finally:  Calculate the small ROTATION and POSITION to the side of the HEAD (like a CAMERA MAN would use the camera...):
                 //
-                Vector3 shoulderOffset = Quaternion.Euler(0, 90, 0) * shootDirection * shoulderOffsetAmount;
+                Vector3 horizontalShoulderOffset = Quaternion.Euler(0, 90, 0) * shootDirection * shoulderOffsetAmount;
                 
                 // Finally:  POSITION the CAMERA:   SUM the Vector3 INVOLVED:
-                // 1- At the 'shooterUnit' Position.
-                // 2- At the SHOULDER HEIGHT  (because (1) was giving us the FEET POSITION: (X, 0.0f, Z)).
-                // 3- (shoulderOffset) POSITION IT slightly to the Right of the shoulder.
-                // 4- We want to see the SHOOTER too, so we Position the whole setup a little behind (shootDirection * -1), to see the system: SHOOTER + TARGET:
+                // 1- At the 'shooterUnit' Position, but still at the ground level, y=0 (shooterUnit.GetWorldPosition()).
+                // 2- At the SHOULDER HEIGHT  (_cameraCharacterHeight - because (1) was giving us the FEET POSITION: (X, 0.0f, Z)).
+                // 3- (horizontalShoulderOffset) To POSITION IT slightly to the Right of the shoulder.
+                // 4- We want to see the SHOOTER too, so we Position the whole setup a little behind the 'Aim Line Vector3' (shootDirection * -1), to see the system: SHOOTER + TARGET:
                 //
-                Vector3 actionCameraPosition = shooterUnit.GetWorldPosition() + _cameraCharacterHeight + shoulderOffset + (shootDirection * -1);
+                Vector3 actionCameraPosition = shooterUnit.GetWorldPosition() + _cameraCharacterHeight + horizontalShoulderOffset + (shootDirection * -1);
                 
                 // Copy the POSITIONAL-calculated Final Vector3:
                 //
