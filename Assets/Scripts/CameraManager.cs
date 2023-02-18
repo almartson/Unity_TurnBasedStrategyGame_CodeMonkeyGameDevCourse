9d5476d8c681e5758c0c1b84189ff8ab7aaac8ef
AlMartson <6304456+almartson@users.noreply.github.com>
2023-02-17 23:27:16 -0500

    * Created the Method: <<BeginTakeCameraShootAction(shootAction)>>  on CameraManager.cs, to organize the actions taken by the camera and, ultimately (in a future commit), simplify all the Math and the Architecture used.

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -121,58 +121,10 @@ public class CameraManager : MonoBehaviour
         {
             case ShootAction shootAction:
                 
-                // Camera SET for the whole SHOOTING ANIMATIONS:
-                // A CloseUp to the Shooter & Target together:
-                
-                // ACTORS... of this Action:
-                // Initiator of this ACTION:
+                // Set the Camera, for this Action:
                 //
-                Unit shooterUnit = shootAction.GetUnit();
-                //
-                // TARGET
-                //
-                Unit targetUnit = shootAction.GetTargetUnit();
-                
-                // Calculate the 'SHOULDER HEIGHT', to put the Camera there:
-                //
-                _cameraCharacterHeight = Vector3.up * _shoulderHeightForUnitCharacter;
-                
-                // Calculate (or get) the SHOOT DIRECTION  (vector)
-                //
-                // ORIGINAL:
-                Vector3 shootDirection = (targetUnit.GetWorldPosition() - shooterUnit.GetWorldPosition()).normalized;
-                // Optimized:
-                //
-                // Use:   shootAction.AimDirection
-                //
-                // Vector3 shootDirection = shootAction.AimDirection;
-                
-                // Calculate:  an small Offset to the Camera Rotation, to look in a 3RD CAMERA PERSPECTIVE towards the Target & Shooter together:
-                // This is the 'Unit shoulder's lenght'... because we want to displace the Camera View (from the center of the Character's Neck...) to the right shoulder:
-                // NOTE: although the exact value, with the current Scale of (1, 1, 1), is: 0.34f.
-                //
-                float shoulderOffsetAmount = 0.5f;
-                //
-                // Finally:  Calculate the small ROTATION and POSITION to the side of the HEAD (like a CAMERA MAN would use the camera...):
-                //
-                Vector3 horizontalShoulderOffset = Quaternion.Euler(0, 90, 0) * shootDirection * shoulderOffsetAmount;
-                
-                // Finally:  POSITION the CAMERA:   SUM the Vector3 INVOLVED:
-                // 1- At the 'shooterUnit' Position, but still at the ground level, y=0 (shooterUnit.GetWorldPosition()).
-                // 2- At the SHOULDER HEIGHT  (_cameraCharacterHeight - because (1) was giving us the FEET POSITION: (X, 0.0f, Z)).
-                // 3- (horizontalShoulderOffset) To POSITION IT slightly to the Right of the shoulder.
-                // 4- We want to see the SHOOTER too, so we Position the whole setup a little behind the 'Aim Line Vector3' (shootDirection * -1), to see the system: SHOOTER + TARGET:
-                //
-                Vector3 actionCameraPosition = shooterUnit.GetWorldPosition() + _cameraCharacterHeight + horizontalShoulderOffset + (shootDirection * -1);
-                
-                // Copy the POSITIONAL-calculated Final Vector3:
-                //
-                _actionCameraGameObject.transform.position = actionCameraPosition;
-                //
-                // Add a ROTATION:   Looking towards the Target.
-                //
-                _actionCameraGameObject.transform.LookAt(targetUnit.GetWorldPosition() + _cameraCharacterHeight);
-                
+                BeginTakeCameraShootAction(shootAction);
+
                 // Show / Initiate the Camera Transitional MOVEMENT:
                 //
                 ShowActionCamera();
@@ -182,7 +134,7 @@ public class CameraManager : MonoBehaviour
         }//End switch (sender)
 
     }// End BaseAction_OnAnyActionStarted
-
+    
     
     /// <summary>
     /// Delegate Listener that is triggered when ANY ACTION ENDS (regardless of the Type, any) (reason: i.e.: this is a STATIC EVENT... in the 'BaseAction' Class).
@@ -211,6 +163,76 @@ public class CameraManager : MonoBehaviour
     }// End BaseAction_OnAnyActionCompleted
 
 
+    
+    #region Take Camera Action
+
+    #region Shooting Animation
+    
+    /// <summary>
+    /// Does the Mathematical calculations when executing the SHOOT ACTION.
+    /// </summary>
+    /// <param name="shootAction"></param>
+    private void BeginTakeCameraShootAction(ShootAction shootAction)
+    {
+        // Camera SET for the whole SHOOTING ANIMATIONS:
+        // A CloseUp to the Shooter & Target together:
+
+        // ACTORS... of this Action:
+        // Initiator of this ACTION:
+        //
+        Unit shooterUnit = shootAction.GetUnit();
+        //
+        // TARGET
+        //
+        Unit targetUnit = shootAction.GetTargetUnit();
+
+        // Calculate the 'SHOULDER HEIGHT', to put the Camera there:
+        //
+        _cameraCharacterHeight = Vector3.up * _shoulderHeightForUnitCharacter;
+
+        // Calculate (or get) the SHOOT DIRECTION  (vector)
+        //
+        // ORIGINAL:
+        Vector3 shootDirection = (targetUnit.GetWorldPosition() - shooterUnit.GetWorldPosition()).normalized;
+        // Optimized:
+        //
+        // Use:   shootAction.AimDirection
+        //
+        // Vector3 shootDirection = shootAction.AimDirection;
+
+        // Calculate:  an small Offset to the Camera Rotation, to look in a 3RD CAMERA PERSPECTIVE towards the Target & Shooter together:
+        // This is the 'Unit shoulder's lenght'... because we want to displace the Camera View (from the center of the Character's Neck...) to the right shoulder:
+        // NOTE: although the exact value, with the current Scale of (1, 1, 1), is: 0.34f.
+        //
+        float shoulderOffsetAmount = 0.5f;
+        //
+        // Finally:  Calculate the small ROTATION and POSITION to the side of the HEAD (like a CAMERA MAN would use the camera...):
+        //
+        Vector3 horizontalShoulderOffset = Quaternion.Euler(0, 90, 0) * shootDirection * shoulderOffsetAmount;
+
+        // Finally:  POSITION the CAMERA:   SUM the Vector3 INVOLVED:
+        // 1- At the 'shooterUnit' Position, but still at the ground level, y=0 (shooterUnit.GetWorldPosition()).
+        // 2- At the SHOULDER HEIGHT  (_cameraCharacterHeight - because (1) was giving us the FEET POSITION: (X, 0.0f, Z)).
+        // 3- (horizontalShoulderOffset) To POSITION IT slightly to the Right of the shoulder.
+        // 4- We want to see the SHOOTER too, so we Position the whole setup a little behind the 'Aim Line Vector3' (shootDirection * -1), to see the system: SHOOTER + TARGET:
+        //
+        Vector3 actionCameraPosition = shooterUnit.GetWorldPosition() + _cameraCharacterHeight + horizontalShoulderOffset +
+                                       (shootDirection * -1);
+
+        // Copy the POSITIONAL-calculated Final Vector3:
+        //
+        _actionCameraGameObject.transform.position = actionCameraPosition;
+        //
+        // Add a ROTATION:   Looking towards the Target.
+        //
+        _actionCameraGameObject.transform.LookAt(targetUnit.GetWorldPosition() + _cameraCharacterHeight);
+
+    }//End BeginTakeCameraShootAction
+    
+    #endregion Shooting Animation
+    
+    #endregion Take Camera Action
+    
     
     #endregion Delegates / Listeners / CallBacks
     
