23ebf886748a15e0b6c9970b8b5e52bea2657002
AlMartson <6304456+almartson@users.noreply.github.com>
2023-02-23 17:06:05 -0500

    * Added my friend Joppe's Debug Console (it is an UnityPackage), to test it out  ( URL:  https://github.com/Dragyn85/InGameDebugLoggs  )

---
+++ InGameDebugLoggs-main/Scripts/Core/LogMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public struct LogMessage {
+    public readonly LogType type;
+    public readonly string typeName;
+    public readonly string timeRecived;
+    public readonly string condition;
+    public readonly string stackTrace;
+
+    public LogMessage(string condition, string stackTrace, LogType type, DateTime time) {
+        this.condition = condition;
+        this.stackTrace = stackTrace;
+        this.type = type;
+        this.typeName = type.ToString();
+        this.timeRecived = time.ToString();
+    }
+}
---
+++ InGameDebugLoggs-main/Scripts/Core/LoggerOutput.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.IO;
+using System;
+using UnityEditor;
+using TMPro;
+
+public class LoggerOutput : MonoBehaviour {
+    static string DEFAULT_PATH = "/Logger";
+    static string FILENAME = "output";
+    static string FILE_EXTENSION = ".txt";
+
+    [SerializeField, HideInInspector] private string customOutputPath;
+
+    public string outputPath => customOutputPath;
+
+    private void AddDebugMessage(LogMessage message) {
+        string newLogEntryAsJson = JsonUtility.ToJson(message, true);
+        string path = GetSavePath();
+        
+        if(!Directory.Exists(path)) {
+            Directory.CreateDirectory(path);
+        }
+        string filePath = $"{path}/{FILENAME}{FILE_EXTENSION}";
+        try {
+            File.AppendAllText(filePath, newLogEntryAsJson);
+        }
+        catch(Exception e) {
+            Debug.LogException(e);
+        }
+    }
+
+    private void OnEnable() {
+        InGameLogger.OnLogRecived += AddDebugMessage;
+    }
+    private void OnDisable() {
+        InGameLogger.OnLogRecived -= AddDebugMessage;
+    }
+
+    public void SetOutputFolder(string path) {
+        customOutputPath = path;
+    }
+
+    public string GetSavePath() {
+        string path = $"{Application.persistentDataPath}{DEFAULT_PATH}{FILENAME}{FILE_EXTENSION}";
+        if(!string.IsNullOrEmpty(customOutputPath)) {
+            path= customOutputPath;
+        }
+        return path;
+    }
+}
