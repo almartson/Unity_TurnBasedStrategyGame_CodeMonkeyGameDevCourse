4cb8a7ecc8ba268ce8da0f7f7a8f1a69fa1833d3
AlMartson <6304456+almartson@users.noreply.github.com>
2024-05-09 03:33:37 -0500

    * New VFX Materialize for 3D Characters Effect: Additional features:
    Worked to add the VFX as a part of the Game Scene + "Perfecting the Particles..."
    
    Done:
    
    1- Adjusted the Colors of the Rings vs. Particles in the VFX.
    
    2- Re-adjusted, (again), the Time (Duration) and Speed of the Rings vs. Particles vs. the Character's Materializing Effect in it's own 'Shader Graph' effect:
    
    3- Created and Saved a New Prefab for using it in the scene, and linked it to every 3D Character's Spawn Method execution (Done for the Player Units... but not for the Enemy A.I. Units).
    
    =================
    
    Todo:
    
    1- Set (enable) in the Main VFX Code (in the 3D Character's Inspector's GameObject) the option: Delete this VFX when done (maybe after a couple of seconds, after it apparently ended).
    
    2- For the "Enemy A.I." case scenario:  Create and Save a New Prefab for using it in the scene, and linked it to every 3D Character's Spawn Method execution (It's currently done for the Player Units... but not for the Enemy A.I. Units).

---
+++ Assets/Scripts/MainGame/VFX/TeleportPortalSpawnEffectVFX/TeleportPortalSpawnEffectControllerForUnits.cs
@@ -0,0 +1,89 @@
+/* NOTE: Modified Unity C# Script Template by Alec AlMartson...
+...on Path:   /PathToUnityHub/Unity/Hub/Editor/UNITY_VERSION_FOR_EXAMPLE__2020.3.36f1/Editor/Data/Resources/ScriptTemplates/81-C# Script-NewBehaviourScript.cs
+*/
+using UnityEngine;
+
+
+public class TeleportPortalSpawnEffectControllerForUnits : BaseVFXShaderValueController
+{
+
+    #region Attributes
+
+    
+    #region Dissolve VFX's: Value and Time Rates
+
+    
+    #endregion Dissolve VFX's: Value and Time Rates
+
+    #endregion Attributes
+
+
+    #region Unity Methods
+
+    /// <summary>
+    /// Awake is called before the Start calls round
+    /// </summary>
+    protected override void Awake()
+    {
+        base.Awake();
+
+    }// End Awake()
+
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    protected void Start()
+    {
+        // Restore the original state of the character
+        //
+        TryUndoVFX();
+            
+        // Start the VFX as a Coroutine
+        //
+        StartCoroutine(DoStartVFX());
+
+    }// End Start()
+
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    private void Update()
+    {
+        // Check to see if the user presses the SPACEBAR Button,
+        //..if so, then enable the VFX.
+        //
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Restore the original state of the character
+            //
+            TryUndoVFX();
+            
+            // Start the VFX as a Coroutine
+            //
+            StartCoroutine(DoStartVFX());
+
+        }//End if (Input.GetKeyDown...
+        
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            // Restore the original state of the character
+            //
+            TryUndoVFX();
+
+        }//End if (Input.GetKeyDown..
+        
+    }// End Update()
+
+    #endregion Unity Methods
+    
+
+    #region My Custom Methods
+    
+    
+    
+
+    #endregion My Custom Methods
+
+}
